209fbae74b8db4a7f166103b8ab544f6cac7a298
Kresna <kresna@usetada.com>
2026-07-12 01:30:34 +0700

    debug: add console logs to worker to trace execution

--- src/tools/demo/hash.worker.ts
+++ src/tools/demo/hash.worker.ts
@@ -1,17 +1,24 @@
 import { expose } from 'comlink';
 import { hashFile } from './hash.lib';
 
+console.log('Hash worker script loaded!');
+
 const api = {
   async hashFile(
     fileBuffer: ArrayBuffer,
     onProgress: (percent: number) => void
   ): Promise<string> {
+    console.log('Worker: hashFile called with buffer size:', fileBuffer.byteLength);
     onProgress(50);
     const hash = await hashFile(fileBuffer);
+    console.log('Worker: hash computed:', hash);
     onProgress(100);
     return hash;
   }
 };
 
 export type HashWorkerAPI = typeof api;
+
+console.log('Worker: Exposing API:', Object.keys(api));
 expose(api);
+console.log('Worker: API exposed via Comlink');
